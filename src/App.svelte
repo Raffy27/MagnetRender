f4fff6749cc72da41dc01642e381796e8dd4e478
Raffy E <raffy27@pm.me>
2021-11-10 17:41:36 +0200

    Starting parameterization

--- src/App.svelte
+++ src/App.svelte
@@ -9,16 +9,33 @@
 	import Preview from './Preview.svelte';
 	export let name;
 	export let version;
+
+	let rp = {
+		type: 0,
+
+		dim: {
+			width: 150,
+			height: 100,
+		},
+
+		fileName: null,
+		fileType: null
+	};
+
+	function render(){
+		console.log('Render params = ');
+		console.log(rp);
+	}
 </script>
 
 <main class="h-full">
 	<div class="container flex flex-col md:flex-row flex-wrap md:flex-nowrap">
 		<div id="left" class="flex flex-col flex-wrap space-y-6 md:w-1/2">
 			<h1>{name} v{version}</h1>
-			<InputForm />
+			<InputForm bind:rp={rp} on:render={render} />
 		</div>
 		<div id="right" class="md:w-1/2 pl-2">
-			<Preview />
+			<Preview bind:rp={rp} />
 		</div>
 	</div>
 </main>
--- src/InputForm.svelte
+++ src/InputForm.svelte
@@ -1,37 +1,38 @@
 <script>
 	import Select from './components/Select.svelte';
 	import Input from './components/Input.svelte';
-	let type;
+	import { createEventDispatcher } from 'svelte';
 
-	let width, height, depth, radius, inner;
-	let fileName, fileType;
+	const dispatch = createEventDispatcher();
+
+	export let rp;
 </script>
 
 <form class="w-full min-w-sm">
-	<Select label="Magnet Type" bind:value={type}>
+	<Select label="Magnet Type" bind:value={rp.type}>
 		<option value={0}>Prism</option>
 		<option value={1}>Ring</option>
 		<option value={2}>Cylinder</option>
 	</Select>
-	{#if type == 0}
-		<Input label="Height" unit="mm" hint="10" bind:value={height} />
-		<Input label="Width" unit="mm" hint="10" bind:value={width} />
-		<Input label="Depth" unit="mm" hint="10" bind:value={depth} />
-	{:else if type == 1}
-		<Input label="Height" unit="mm" hint="10" bind:value={height} />
-		<Input label="Radius" unit="mm" hint="5" bind:value={radius} />
-		<Input label="Inner Radius" unit="mm" hint="3" bind:value={inner} />
-	{:else if type == 2}
-		<Input label="Radius" unit="mm" hint="5" bind:value={radius} />
-		<Input label="Height" unit="mm" hint="10" bind:value={height} />
+	{#if rp.type == 0}
+		<Input label="Height" unit="mm" hint="10" bind:value={rp.dim.height} />
+		<Input label="Width" unit="mm" hint="10" bind:value={rp.dim.width} />
+		<Input label="Depth" unit="mm" hint="10" bind:value={rp.dim.depth} />
+	{:else if rp.type == 1}
+		<Input label="Height" unit="mm" hint="10" bind:value={rp.dim.height} />
+		<Input label="Radius" unit="mm" hint="5" bind:value={rp.dim.radius} />
+		<Input label="Inner Radius" unit="mm" hint="3" bind:value={rp.dim.inner} />
+	{:else if rp.type == 2}
+		<Input label="Radius" unit="mm" hint="5" bind:value={rp.dim.radius} />
+		<Input label="Height" unit="mm" hint="10" bind:value={rp.dim.height} />
 	{/if}
 
 	<div class="flex flex-wrap -mx-2 mb-2">
 		<div class="w-full md:w-2/3 px-2">
-			<Input label="File Name" bind:value={fileName} />
+			<Input label="File Name" bind:value={rp.fileName} />
 		</div>
 		<div class="w-full md:w-1/3 px-2">
-			<Select label="File Type" bind:value={fileType}>
+			<Select label="File Type" bind:value={rp.fileType}>
 				<option value={0}>SVG</option>
 				<option value={1}>JPG</option>
 				<option value={2}>PNG</option>
@@ -43,7 +44,8 @@
 		<button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
 			Export
 		</button>
-		<button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
+		<button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+			on:click|preventDefault={() => dispatch('render')}>
 			Preview
 		</button>
   	</div>
